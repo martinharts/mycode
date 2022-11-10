3a234b63565bfe1a4bacbac175cd1492e1703f1d
martinharts <martin_harts@yahoo.com>
2022-11-10 22:33:00 +0000

    join and split

---
+++ lab_input/challenge18.py
@@ -0,0 +1,14 @@
+# input asking users name
+
+name = input("What is your name?")
+
+
+# input asking day of the week
+
+day = input ("What day of the week is it?")
+
+
+# print out results
+
+print("Hello, ", name, "! Happy", day, "!")
+
